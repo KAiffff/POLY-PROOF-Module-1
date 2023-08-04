260691547007a0b845e5f5dd044aae667343f79c
Moh Kaif <136920500+KAiffff@users.noreply.github.com>
2023-08-05 03:49:26 +0530

    nfts.js

---
+++ Metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "Most Popular Indian Cars",
+    image: "QmfWJM4krzhHJdtkAVg2dtKRh8NPTqNYwB55Hrxb1BpzBK",
+  },
+  {
+    name: "1",
+    description: "Most Popular Indian Cars",
+    image: "QmSN5fcE6SFFbhddMUdhwrxkXb2wsAQMkY7NaX7obmQGRS",
+  }, //change
+  {
+    name: "2",
+    description: "Most Popular Indian Cars",
+    image: "QmUn1qpRPh3XVbd2FYi8evT2wSxMFV3uKpnAzEbqJEBSvW",
+  },
+  {
+    name: "3",
+    description: "Most Popular Indian Cars",
+    image: "QmTNNmih5zBUJEf4mNHzqeWiH9VxNAZUaHtCLSQtBwXAZQ",
+  },
+  {
+    name: "4",
+    description: "Most Popular Indian Cars.",
+    image: "QmdVnH2dJY3xYCMncpJmt8WCcETqNJgwZ5eEZqqhyTwkF4",
+  },
+];
+
+module.exports = nfts;
